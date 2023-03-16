972a11b6f1804e6fc3ff9013c4cac1e72af99199
zakaria kamili <zakariakamili97@gmail.com>
2023-03-16 21:40:48 +0000

    create new method OldMethod

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
@@ -17,6 +18,18 @@ class MoviesController extends AbstractController
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/MoviesController.php',
+        ]);
+    }
+    
+    /**
+     * oldMethod
+     * 
+     * @Route("/old",name="old")
+     */
+    public function olMethod():Response {
+        return $this->json([
+             'message' => 'Old Method.',
+             'path'=> 'src/Controller/MoviesController.php',
         ]);
     }
 }
