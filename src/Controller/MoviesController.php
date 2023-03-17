920d35818bedcf7a8621569444f68a5781c55eba
zakaria kamili <zakariakamili97@gmail.com>
2023-03-17 14:34:53 +0000

    fix routing problem

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -10,26 +10,30 @@ use Symfony\Component\Routing\Annotation\Route;
 class MoviesController extends AbstractController
 {
     /**
-     * @Route("/movies", name="app_movies")
+     * @Route("/movies/{name}", name="app_movies")
      */
-    #[Route("/movies/{name}", name:"movies")]
+    // #[Route("/movies/inception", name:"movies")]
     public function index(): JsonResponse
     {
+
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/MoviesController.php',
         ]);
+
     }
+
+
     
     /**
      * oldMethod
      * 
-     * @Route("/old",name="old")
+     * @Route("/old/test",name="old")
      */
-    // public function olMethod():Response {
-    //     return $this->json([
-    //          'message' => 'Old Method.',
-    //          'path'=> 'src/Controller/MoviesController.php',
-    //     ]);
-    // }
+    public function olMethod():Response {
+        return $this->json([
+             'message' => 'Old Method.',
+             'path'=> 'src/Controller/MoviesController.php',
+        ]);
+    }
 }
